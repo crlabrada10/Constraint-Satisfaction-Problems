a5b5ff1a5d781d8632d4b34c4be7a822e076cafe
Carlos R. Labrada <crlabrada10@gmail.com>
2021-03-07 21:19:36 -0300

    first commit

---
+++ src/main/java/com/home/csp/App.java
@@ -0,0 +1,13 @@
+package com.home.csp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
